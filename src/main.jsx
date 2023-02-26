ef1410b186e329a759498090a1c00a66923ca95f
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-02-26 13:40:16 +0200

    Install React Query and use it to fetch Pet Details 🌍🚀

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import Details from "./Details.jsx";
 import "./index.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <header>
-        <Link to={"/"}>Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to={"/"}>Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
